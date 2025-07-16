fe4aa7bb650bb4420af50a81146d66b314523998
Kato <zzeroyzz@gmail.com>
2025-07-16 18:02:22 -0400

    lint

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,5 +1,7 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
 /* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
