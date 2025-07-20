c3e81dadf1fad0e15ba6721cd2fb78ffd84f30f9
Kato <zzeroyzz@gmail.com>
2025-07-20 09:44:09 -0400

    feat: added dotlottie type

---
+++ src/types/dotlottie-player.d.ts
@@ -0,0 +1,12 @@
+declare namespace JSX {
+  interface IntrinsicElements {
+    'dotlottie-player': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
+      src?: string;
+      background?: string;
+      speed?: number;
+      loop?: boolean;
+      autoplay?: boolean;
+      style?: React.CSSProperties;
+    };
+  }
+}
