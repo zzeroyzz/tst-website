411e4a4f65f8290d6205f61f3d06fe3508403608
Kato <zzeroyzz@gmail.com>
2025-07-22 16:52:29 -0400

    feat: added skeleton loading

---
+++ src/components/skeleton/index.ts
@@ -0,0 +1,21 @@
+// components/skeleton/index.ts
+
+// Since all components are in one file, import them all from that file
+// Assuming your large file is named 'SkeletonComponents.tsx'
+export {
+  Skeleton,
+  SkeletonText,
+  SkeletonCard,
+  SkeletonAvatar,
+  SkeletonButton,
+  SkeletonBreadcrumb,
+  SkeletonHeader,
+  SinglePostSkeleton,
+  NewsletterArchiveSkeleton,
+  NewsletterViewSkeleton,
+  LeadsViewSkeleton,
+  DashboardViewSkeleton,
+  KanbanBoardSkeleton,
+} from './SkeletonBase.tsx';
+
+// OR if you named the file differently, adjust the import path accordingly
