9281ee2f252b9709012392df66e8cce66769f86f
Kato <zzeroyzz@gmail.com>
2025-07-22 17:05:51 -0400

    feat: updated images

--- src/components/ProfileImage.tsx
+++ src/components/ProfileImage.tsx
@@ -4,12 +4,12 @@ import Image from "next/image";
 const ProfileImage = () => {
   return (
 
-    <div className="max-w-xs mx-auto">
+    <div className="max-w-xl mx-auto">
       <Image
-        src="https://pvbdrbaquwivhylsmagn.supabase.co/storage/v1/object/public/tst-assets/website%20assets/MYT.svg"
-        alt="Kay, your therapist"
-        width={400}
-        height={450}
+        src="https://pvbdrbaquwivhylsmagn.supabase.co/storage/v1/object/public/tst-assets/website%20assets/MYT-v2.png"
+        alt="Kay, your favorite therapist"
+        width={600}
+        height={600}
         className="w-full h-auto"
       />
     </div>
