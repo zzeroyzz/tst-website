879c9b122794989d6f0b4eb56c829968e7b279aa
Kato <zzeroyzz@gmail.com>
2025-08-01 15:38:55 -0400

    rm:babel

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-// babel.config.js (create this file in your root directory)
-module.exports = {
-  presets: [
-    ['next/babel'],
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    ['@babel/preset-react', { runtime: 'automatic' }],
-    '@babel/preset-typescript'
-  ],
-}
