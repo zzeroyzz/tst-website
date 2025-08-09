e7b6d800f4f74b3beba88161cc4715e4c2b301d5
Kato <zzeroyzz@gmail.com>
2025-08-09 12:11:44 -0400

    feat: added conditional layouts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { Toaster } from "react-hot-toast";
 import { Analytics } from "@vercel/analytics/next"
 import CookieConsent from "@/components/CookieConsent/CookieConsent";
 import Script from 'next/script';
+import ConditionalLayout from '@/components/ConditionalLayout/ConditionalLayout';
 
 const workSans = Work_Sans({
   subsets: ["latin"],
@@ -50,7 +51,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Add the schema scripts to the head */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteSchema) }}
@@ -60,39 +60,42 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(medicalBusinessSchema) }}
         />
 
-<Script
-  id="gtm-init"
-  strategy="afterInteractive"
-  dangerouslySetInnerHTML={{
-    __html: `
-      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-      'https://www.googletagmanager.com/gtm.js?id=GTM-N6VXX6CK'+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','GTM-N6VXX6CK');
-    `,
-  }}
-/>
+        <Script
+          id="gtm-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=GTM-N6VXX6CK'+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-N6VXX6CK');
+            `,
+          }}
+        />
       </head>
       <body
         className={`${workSans.variable} font-sans bg-tst-cream text-black antialiased`}
       >
         <Toaster position="top-center" reverseOrder={false} />
-        <Nav />
-        <main>{children}
-           <Analytics />
-        </main>
-        <Footer />
+
+        <ConditionalLayout>
+          <Nav />
+          <main>{children}
+            <Analytics />
+          </main>
+          <Footer />
+        </ConditionalLayout>
 
         <CookieConsent />
-<noscript>
-  <iframe
-    src="https://www.googletagmanager.com/ns.html?id=GTM-N6VXX6CK"
-    height="0"
-    width="0"
-    style={{ display: 'none', visibility: 'hidden' }}
-  />
-</noscript>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N6VXX6CK"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
       </body>
     </html>
   );
