14d3772bef612b50648846abb4bc393028956d95
Kato <zzeroyzz@gmail.com>
2025-09-02 08:53:42 -0400

    updated sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -94,12 +94,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
       priority: 0.95,
     },
     {
-      url: `${baseUrl}/book/affirming`,
-      lastModified: nowIso,
-      changeFrequency: 'monthly',
-      priority: 0.9,
-    },
-    {
       url: `${baseUrl}/book/nd`,
       lastModified: nowIso,
       changeFrequency: 'monthly',
